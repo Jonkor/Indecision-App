ddab90462a5a640ae6ffc7ad14f4784c27778a4d
Jonkor <juan.kucho@hotmail.com>
2025-07-21 10:57:36 -0700

    add jsx and events

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,37 @@
 "use strict";
 
 console.log('App.js is running');
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info',
+  options: ['One', 'Two']
+};
 
 // JSX - JavaScript XML
-var template = /*#__PURE__*/React.createElement("h1", null, "Indecision App");
+
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options' : 'No options'), /*#__PURE__*/React.createElement("o1", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
+var count = 0;
+var addOne = function addOne() {
+  count += 1;
+  renderCounterApp();
+};
+var minusOne = function minusOne() {
+  count -= 1;
+  renderCounterApp();
+};
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+};
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Count: ", count), /*#__PURE__*/React.createElement("button", {
+    onClick: addOne
+  }, "+1"), /*#__PURE__*/React.createElement("button", {
+    onClick: minusOne
+  }, "-1"), /*#__PURE__*/React.createElement("button", {
+    onClick: reset
+  }, "Reset"));
+  ReactDOM.render(templateTwo, appRoot);
+};
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,8 +1,53 @@
 console.log('App.js is running');
 
+const app = {
+    title: 'Indecision App',
+    subtitle: 'This is some info',
+    options: ['One', 'Two']
+}
+
 // JSX - JavaScript XML
-let template = <h1>Indecision App</h1>;
 
-let appRoot = document.getElementById('app');
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
+        <o1>
+            <li>Item one</li>
+            <li>Item two</li>
+        </o1>
+    </div>
+);
 
-ReactDOM.render(template, appRoot);
+let count = 0;
+const addOne = () => {
+    count += 1;
+    renderCounterApp();
+};
+
+const minusOne =  () => {
+    count -= 1;
+    renderCounterApp();
+};
+
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
+
+const appRoot = document.getElementById('app');
+
+const renderCounterApp = () => {
+    const templateTwo = (
+    <div> 
+        <h1>Count: {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>Reset</button>    
+    </div>
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
